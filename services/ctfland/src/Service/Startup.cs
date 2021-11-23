2e4d4e84f092a7afeb8aa110e1e8572845828190
Leofwin <andy_namor@mail.ru>
2021-11-23 18:37:54 +0500

    Не использую HTTPS

--- services/ctfland/src/Service/Startup.cs
+++ services/ctfland/src/Service/Startup.cs
@@ -60,10 +60,8 @@ namespace CtfLand.Service
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
