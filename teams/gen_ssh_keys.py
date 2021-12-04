e710ec4fcb4163f3c6f84b52d140b484fdcfb81a
Alex Ankudinov <xelez0@gmail.com>
2021-12-04 22:59:54 +0500

    fresh secrets

--- teams/gen_ssh_keys.py
+++ teams/gen_ssh_keys.py
@@ -3,9 +3,9 @@ from os import system as r
 import random
 import string
 
-r("ssh-keygen -t rsa -N '' -f {}/for_devs.ssh_key".format(i))
+r("ssh-keygen -t rsa -N '' -f for_devs.ssh_key")
 
-for i in range(101, 105):
+for i in range(101, 111):
     r('mkdir -p {}'.format(i))
     r("ssh-keygen -t rsa -N '' -f {}/ssh_key".format(i))
 
